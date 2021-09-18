8fdb286fe1dcd41596685e8d654555bb7e6f7d81
Pranav Rao <56097527+pranavrao145@users.noreply.github.com>
2021-09-18 14:48:24 -0400

    style(routes): fixed formatting on routes files

--- server/src/routes/groups.ts
+++ server/src/routes/groups.ts
@@ -1,14 +1,12 @@
-import exp from 'constants';
 import express from 'express';
-import { get } from 'https';
 import { createGroup, deleteGroup, getAllGroups, getGroup, updateGroup } from '../controllers/groups';
 
 const router = express.Router();
-router.get("/:uid",getGroup)
-router.get("/",getAllGroups)
+
+router.get("/:uid", getGroup)
+router.get("/", getAllGroups)
 router.post("/", createGroup)
 router.put("/:uid", updateGroup)
-router.delete("//:uid",deleteGroup)
+router.delete("/:uid", deleteGroup)
 
 export default router;
-
--- server/src/routes/log.ts
+++ server/src/routes/log.ts
@@ -1,14 +1,12 @@
-import exp from 'constants';
 import express from 'express';
-import { get } from 'https';
 import { createLog, deleteLog, getAllLogs, getLog, updateLog } from '../controllers/logs';
 
 const router = express.Router();
-router.get("/:uid",getLog)
-router.get("/",getAllLogs)
+
+router.get("/:uid", getLog)
+router.get("/", getAllLogs)
 router.post("/", createLog)
 router.put("/:uid", updateLog)
-router.delete("//:uid",deleteLog)
+router.delete("/:uid", deleteLog)
 
 export default router;
-
