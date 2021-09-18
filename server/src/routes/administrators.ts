8ae22f75507308257207ceab2a6b71ce5be507b5
Lavansurendra <lavansurendra@gmail.com>
2021-09-18 18:20:15 -0400

    feat(routes): added new api routes

--- server/src/routes/administrators.ts
+++ server/src/routes/administrators.ts
@@ -1,5 +1,5 @@
 import express, { Router } from 'express';
-import { createAdministrator, deleteAdministrator, getAdministrator, getAllAdministrators, updateAdministrator } from '../controllers/administrators';
+import { createAdministrator, deleteAdministrator, getAdministrator, getAllAdministrators, updateAdministrator, getAdminOrganizations } from '../controllers/administrators';
 
 const router: Router = express.Router();
 
@@ -8,5 +8,6 @@ router.get("/:firebase_uid", getAdministrator)
 router.post("/", createAdministrator)
 router.put("/:firebase_uid", updateAdministrator)
 router.delete("/:firebase_uid", deleteAdministrator)
+router.get("/:id/admins", getAdminOrganizations)
 
 export default router;
