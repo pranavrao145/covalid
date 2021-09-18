7a3b0f27133c9181e5c7d7c27783e4d3f07a9b50
Lavansurendra <lavansurendra@gmail.com>
2021-09-18 14:41:21 -0400

    changing route names

--- server/src/routes/questionnaires.ts
+++ server/src/routes/questionnaires.ts
@@ -1,14 +1,14 @@
-import exp from 'constants';
+
 import express from 'express';
-import { get } from 'https';
+
 import { createQuestionnaire, deleteQuestionnaire, getAllQuestionnaires, getQuestionnaire, updateQuestionnaire } from '../controllers/questionnaires';
 
 const router = express.Router();
-router.get("/questionnaires/:uid",getQuestionnaire)
-router.get("/questionnaires",getAllQuestionnaires)
-router.post("/questionnaires", createQuestionnaire)
-router.put("/questionnaires/:uid", updateQuestionnaire)
-router.delete("/questionnaires/:uid",deleteQuestionnaire)
+router.get("/:uid",getQuestionnaire)
+router.get("/",getAllQuestionnaires)
+router.post("/", createQuestionnaire)
+router.put("/:uid", updateQuestionnaire)
+router.delete("/:uid",deleteQuestionnaire)
 
 export default router;
 
