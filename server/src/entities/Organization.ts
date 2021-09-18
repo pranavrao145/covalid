f1f4614a266ef66f9f5e1f07ebe9219b1844d2f9
Pranav Rao <56097527+pranavrao145@users.noreply.github.com>
2021-09-17 23:34:54 -0400

    feat(database): set up db relation between organizations and groups
    A one to many relationship was created. An organization can have many
    groups, and a group can only belong to one organization.

--- server/src/entities/Organization.ts
+++ server/src/entities/Organization.ts
@@ -1,4 +1,5 @@
-import { PrimaryGeneratedColumn, Column, Entity } from 'typeorm'
+import { PrimaryGeneratedColumn, Column, Entity, OneToMany } from 'typeorm'
+import { Group } from './Group';
 
 @Entity()
 export class Organization {
@@ -13,4 +14,6 @@ export class Organization {
 
     @Column({ type: 'timestamptz' })
     date_created!: Date;
+
+    @OneToMany(() => Group, group => group.organization) groups!: Group[];
 }
