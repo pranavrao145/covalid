acbcc1a840063237df19b09b346edbaafa833ae4
Lavansurendra <lavansurendra@gmail.com>
2021-09-18 14:41:21 -0400

    changing route names

--- server/src/routes/users.ts
+++ server/src/routes/users.ts
@@ -1,13 +1,13 @@
-import exp from 'constants';
+
 import express from 'express';
-import { get } from 'https';
+
 import { getUser,getAllUsers,updateUser,deleteUser } from '../controllers/users';
 
 const router = express.Router();
-router.get("/users", getAllUsers)
-router.get("/users/:uid", getUser)
-router.put("/users/:uid", updateUser)
-router.delete("/users/:uid", deleteUser)
+router.get("/", getAllUsers)
+router.get("/:uid", getUser)
+router.put("/:uid", updateUser)
+router.delete("/:uid", deleteUser)
 
 export default router;
 
