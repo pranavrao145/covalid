ada761da5b96a9819495e5e18264bd7844ba0ae6
Lavansurendra <lavansurendra@gmail.com>
2021-09-18 14:41:21 -0400

    changing route names

--- server/src/routes/members.ts
+++ server/src/routes/members.ts
@@ -1,14 +1,14 @@
-import exp from 'constants';
+
 import express from 'express';
-import { get } from 'https';
+
 import { createMember, deleteMember, getAllMembers, getMember, updateMember } from '../controllers/members';
 
 const router = express.Router();
-router.get("/members/:uid",getMember)
-router.get("/members",getAllMembers)
-router.post("/members", createMember)
-router.put("/members/:uid", updateMember)
-router.delete("/members/:uid",deleteMember)
+router.get("/:uid",getMember)
+router.get("/",getAllMembers)
+router.post("/", createMember)
+router.put("/:uid", updateMember)
+router.delete("/:uid",deleteMember)
 
 export default router;
 
