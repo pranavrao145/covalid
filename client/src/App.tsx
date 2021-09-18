39c2d8413576ba00db176b4c37b8e17154727fb6
Futuristick <me@futuristick.ca>
2021-09-17 23:55:01 -0400

    MUI table styling attempts

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,81 @@
 import React from "react";
+import { GridColDef, GridRowModel, GridRowParams } from "@mui/x-data-grid";
+import DataGrid from "./components/DataGrid";
 
-const App: React.FC = () => <span>Hello world</span>;
+const App: React.FC = () => {
+	const columns = [
+		{
+			field: "id",
+		},
+		{
+			field: "name",
+			headerName: "Name",
+			flex: 1,
+		},
+		{
+			field: "groups",
+			headerName: "Groups",
+			flex: 1,
+		},
+		{
+			field: "entryTime",
+			headerName: "Entry Time",
+			type: "dateTime",
+			flex: 1,
+		},
+		{
+			field: "cleared",
+			headerName: "Cleared",
+			type: "boolean",
+			flex: 1,
+		},
+		{
+			field: "actions",
+			headerName: "Actions",
+			type: "actions",
+			flex: 2,
+			getActions: (params: GridRowParams) => [<button>hi</button>],
+		},
+	] as GridColDef[];
+
+	const rows: GridRowModel[] = [
+		{
+			id: 1,
+			name: "Jane Cooper",
+			groups: "Ms. Smith's Class, Mr. Jones's Class",
+			entryTime: new Date("2021-01-17T09:44:10"),
+			cleared: true,
+		},
+		{
+			id: 2,
+			name: "Jane Cooper",
+			groups: "Ms. Smith's Class, Mr. Jones's Class",
+			entryTime: new Date("2021-01-17T09:44:10"),
+			cleared: true,
+		},
+		{
+			id: 3,
+			name: "Jane Cooper",
+			groups: "Ms. Smith's Class, Mr. Jones's Class",
+			entryTime: new Date("2021-01-17T09:44:10"),
+			cleared: true,
+		},
+		{
+			id: 4,
+			name: "Jane Cooper",
+			groups: "Ms. Smith's Class, Mr. Jones's Class",
+			entryTime: new Date("2021-01-17T09:44:10"),
+			cleared: true,
+		},
+	];
+
+	return (
+		<div className="flex h-96 w-[80vw] m-32">
+			<div className="flex-grow">
+				<DataGrid disableSelectionOnClick columns={columns} rows={rows} />
+			</div>
+		</div>
+	);
+};
 
 export default App;
