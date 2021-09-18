e8dd8e8895c495d1983c3ea79f064e4a540876ea
Pranav Rao <56097527+pranavrao145@users.noreply.github.com>
2021-09-18 12:37:37 -0400

    feat(controllers): added managers controller
    All of the basic REST actions were added.

---
+++ server/src/controllers/managers.ts
@@ -0,0 +1,103 @@
+import { Request, Response } from "express";
+import { createConnection } from "typeorm";
+import { Manager } from "../entities/Manager";
+
+// GET /managers/:firebase_id
+export const getManager = async (req: Request, res: Response) => {
+    // get a connection to the database
+    const connection = await createConnection();
+
+    // access the manager repository
+    const repository = connection.getRepository(Manager);
+
+    try {
+        // get the manager with the uid specified
+        const manager = await repository.findOne({ firebase_uid: req.params.firebase_uid });
+        res.status(200).json(manager);
+    } catch (e) {
+        res.status(404).json({ message: "An error occurred fetching this manager." });
+    }
+}
+
+// GET /managers
+export const getAllManagers = async (_req: Request, res: Response) => {
+    // get a connection to the database
+    const connection = await createConnection();
+
+    // access the manager repository
+    const repository = connection.getRepository(Manager);
+
+    try {
+        // get all the managers in the database
+        const managers = await repository.find();
+        res.status(200).json(managers);
+    } catch (e) {
+        res.status(404).json({ message: "An error occurred fetching all managers." });
+    }
+}
+
+// POST /managers
+export const createManager = async (req: Request, res: Response) => {
+    // get a connection to the database
+    const connection = await createConnection();
+
+    // access the manager repository
+    const repository = connection.getRepository(Manager);
+
+    try {
+        // create a new manager with the given information
+        const manager = repository.create(req.body);
+
+        // save the new manager
+        const saveResults = await repository.save(manager!);
+
+        res.status(200).json(saveResults);
+    } catch (e) {
+        res.status(404).json({ message: "An error occurred creating this manager." });
+    }
+}
+
+// PUT /managers/:firebase_uid
+export const updateManager = async (req: Request, res: Response) => {
+    // get a connection to the database
+    const connection = await createConnection();
+
+    // access the manager repository
+    const repository = connection.getRepository(Manager);
+
+    try {
+        // get the manager with the uid specified
+        const manager = await repository.findOne({ firebase_uid: req.params.firebase_uid });
+
+        // update the manager data
+        repository.merge(manager!, req.body);
+
+        // save the manager with the new details
+        const saveResults = await repository.save(manager!);
+
+        res.status(200).json(saveResults);
+    } catch (e) {
+        res.status(404).json({ message: "An error occurred updating this manager." });
+    }
+}
+
+// DELETE /managers/:firebase_uid
+export const deleteManager = async (req: Request, res: Response) => {
+    // get a connection to the database
+    const connection = await createConnection();
+
+    // access the manager repository
+    const repository = connection.getRepository(Manager);
+
+    try {
+        // get the manager with the uid specified
+        const manager = await repository.findOne({ firebase_uid: req.params.firebase_uid });
+
+        // delete the manager 
+        const saveResults = await repository.delete(manager!);
+
+        res.status(200).json(saveResults);
+    } catch (e) {
+        res.status(404).json({ message: "An error occurred deleting this manager." });
+    }
+}
