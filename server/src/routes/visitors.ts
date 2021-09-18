b42cdcdc2b962a3fa292a7faaf8f4d68d239c3de
Lavansurendra <lavansurendra@gmail.com>
2021-09-18 14:41:21 -0400

    changing route names

--- server/src/routes/visitors.ts
+++ server/src/routes/visitors.ts
@@ -1,14 +1,14 @@
-import exp from 'constants';
+
 import express from 'express';
-import { get } from 'https';
+
 import { createVisitor, deleteVisitor, getAllVisitors, getVisitor, updateVisitor } from '../controllers/visitors';
 
 const router = express.Router();
-router.get("/visitors/:uid",getVisitor)
-router.get("/visitors",getAllVisitors)
-router.post("/visitors", createVisitor)
-router.put("/visitors/:uid", updateVisitor)
-router.delete("/visitors/:uid",deleteVisitor)
+router.get("/:uid",getVisitor)
+router.get("/",getAllVisitors)
+router.post("/", createVisitor)
+router.put("/:uid", updateVisitor)
+router.delete("/:uid",deleteVisitor)
 
 export default router;
 
