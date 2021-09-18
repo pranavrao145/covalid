7290247026ba61803dee2ab1551c396fab1e1582
Pranav Rao <56097527+pranavrao145@users.noreply.github.com>
2021-09-18 09:30:29 -0400

    feat(database): set up database connection
    The application now connects to a cockroachdb database in the clouds
    with the correct credentials file.

---
+++ server/src/utils/database.ts
@@ -0,0 +1,22 @@
+import { Connection, ConnectionOptions, createConnection } from "typeorm";
+
+export async function connectToDB(): Promise<Connection> {
+    // set database configuration
+    const connectionOptions: ConnectionOptions = {
+        type: "cockroachdb",
+        host: process.env.DATABASE_HOST,
+        port: parseInt(process.env.DATABASE_PORT!),
+        username: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASS,
+        database: process.env.DATABASE_DATABASE,
+        ssl: true,
+        extra: {
+            ssl: {
+                rejectUnauthorized: false
+            }
+        }
+    }
+
+    // attempt to connect to the database
+    return await createConnection(connectionOptions);
+}
