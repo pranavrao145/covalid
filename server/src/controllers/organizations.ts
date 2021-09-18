dcaaf17d8d35220bf57b8d6855254ea8af0f9798
Pranav Rao <56097527+pranavrao145@users.noreply.github.com>
2021-09-18 19:32:21 -0400

    feat(api): added endpoint to add and delete an administrator to and from an org
    This is untested, but likely works.

--- server/src/controllers/organizations.ts
+++ server/src/controllers/organizations.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { createConnection } from "typeorm";
+import { Administrator } from "../entities/Administrator";
 import { Organization } from "../entities/Organization";
 
 // GET /organizations/:id
@@ -92,7 +93,7 @@ export const deleteOrganization = async (req: Request, res: Response) => {
     try {
         // get the organization with the uid specified
         const organization = await repository.findOne(req.params.id);
- 
+
         // delete the organization 
         const saveResults = await repository.delete(organization!);
 
@@ -139,3 +140,100 @@ export const getOrganizationAdministrators = async (req: Request, res: Response)
         res.status(404).json({ message: "An error occurred fetching this organization's administrators." });
     }
 }
+
+// POST /organization/:id/add_administrator/:firebase_uid
+export const addAdministratorToOrganization = async (req: Request, res: Response) => {
+    // get a connection to the database
+    const connection = await createConnection();
+
+    // access the organization repository
+    const organizationRepository = connection.getRepository(Organization);
+
+    // access the administrator repository
+    const administratorRepository = connection.getRepository(Administrator);
+
+    let organization: any;
+
+    try {
+        // get the organization with the id specified
+        organization = await organizationRepository.findOne(req.params.id);
+    } catch (e) {
+        res.status(404).json({ message: "An error occurred adding the administrator to the organization." });
+    }
+
+    let administrator: any;
+
+    try {
+        // get the administrator with the uid specified
+        administrator = await administratorRepository.findOne({ firebase_uid: req.params.firebase_uid });
+    } catch (e) {
+        res.status(404).json({ message: "An error occurred adding the administrator to the organization." });
+    }
+
+    // add the administrator to the organization
+    organization!.administrators.push(administrator!);
+
+    // add the organization to the administrator
+    administrator!.organizations.push(organization!);
+
+    try {
+        // save the administrator and the organization
+        await organizationRepository.save(organization!);
+        await administratorRepository.save(administrator!);
+
+        res.status(200).json({ message: "Success!" });
+    } catch (e) {
+        res.status(404).json({ message: "An error occurred adding the administrator to the organization." });
+    }
+}
+
+// POST /organization/:id/remove_administrator/:firebase_uid
+export const removeAdministratorFromOrganization = async (req: Request, res: Response) => {
+    // get a connection to the database
+    const connection = await createConnection();
+
+    // access the organization repository
+    const organizationRepository = connection.getRepository(Organization);
+
+    // access the administrator repository
+    const administratorRepository = connection.getRepository(Administrator);
+
+    let organization: any;
+
+    try {
+        // get the organization with the id specified
+        organization = await organizationRepository.findOne(req.params.id);
+    } catch (e) {
+        res.status(404).json({ message: "An error occurred removing the administrator from the organization." });
+    }
+
+    let administrator: any;
+
+    try {
+        // get the administrator with the uid specified
+        administrator = await administratorRepository.findOne({ firebase_uid: req.params.firebase_uid });
+    } catch (e) {
+        res.status(404).json({ message: "An error occurred removing the administrator from the organization." });
+    }
+
+    // remove the administrator from the organization
+    organization!.administrators.forEach((item: any, index: any) => {
+        if (item === administrator) organization!.administrators.splice(index, 1);
+    });
+
+    // remove the organization from the administrator
+    administrator!.organizations.forEach((item: any, index: any) => {
+        if (item === organization) administrator!.organizations.splice(index, 1);
+    });
+
+    try {
+        // save the administrator and the organization
+        await organizationRepository.save(organization!);
+        await administratorRepository.save(administrator!);
+
+        res.status(200).json({ message: "Success!" });
+    } catch (e) {
+        res.status(404).json({ message: "An error occurred removing the administrator from the organization." });
+    }
+}
+
