08b49a098de3d136baa21ebb8bec35450dc72710
Pranav Rao <56097527+pranavrao145@users.noreply.github.com>
2021-09-18 09:30:29 -0400

    feat(database): set up database connection
    The application now connects to a cockroachdb database in the clouds
    with the correct credentials file.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,14 @@
 import express, { Application, Request, Response } from 'express'
 import dotenv from 'dotenv'
+import { connectToDB } from './utils/database';
 
 // environment configuration
 dotenv.config();
-const port = process.env.PORT || 5000;
+
+const PORT = process.env.PORT || 5000;
+
+// connect to database using a helper function
+connectToDB();
 
 // initializing the express app
 const app: Application = express();
@@ -11,12 +16,12 @@ const app: Application = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', async (req: Request, res: Response): Promise<Response> => {
+app.get('/', async (_req: Request, res: Response): Promise<Response> => {
     return res.status(200).send({
         message: 'This application works.'
     });
 });
 
-app.listen(port, (): void => {
-    console.log(`Listening on port ${port}`);
+app.listen(PORT, (): void => {
+    console.log(`Listening on port ${PORT}`);
 });
