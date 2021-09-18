1757383fe7db64c5e58c72babc664f69399f9173
Futuristick <me@futuristick.ca>
2021-09-18 14:13:42 -0400

    Automatic sidebar routing, manage users page

--- client/src/components/dashboard/tables/UserListTable.tsx
+++ client/src/components/dashboard/tables/UserListTable.tsx
@@ -7,7 +7,6 @@ import {
 	GridRowParams,
 } from "@mui/x-data-grid";
 import { Autocomplete, TextField, Chip } from "@mui/material";
-import Badge from "../../ui/Badge";
 import Button from "../../ui/Button";
 import DataGrid from "../../ui/DataGrid";
 import "./CustomTable.css";
@@ -15,27 +14,15 @@ import "./CustomTable.css";
 interface TableEntry {
 	id: number;
 	name: string;
+	email: string;
 	groups: string[];
-	dateCreated: Date;
-	cleared: boolean;
-	answers?: {
-		vaccinated_or_had_covid?: boolean;
-		recently_travelled?: boolean;
-		doctor_told_to_quarantine?: boolean;
-		has_symptoms?: boolean;
-		tested_positive?: boolean;
-	};
 }
-
-const renderClearedBadge = ({ value }: { value: boolean }) => (
-	<Badge className={value ? "" : "bg-rose-100 text-rose-800"}>{value ? "Yes" : "No"}</Badge>
-);
 
 const renderGroupsInput = ({ value }: GridRenderCellParams | GridRenderEditCellParams, editable: boolean) => (
 	<Autocomplete
 		multiple
 		disabled={!editable}
-		limitTags={1} // @ts-ignore
+		limitTags={3} // @ts-ignore
 		defaultValue={value.split("|,")}
 		renderInput={(params) => <TextField {...params} variant="standard" />}
 		options={(value as string).split("|,")}
@@ -59,33 +46,25 @@ const EntryLogTable: React.FC<{
 				flex: 1,
 			},
 			{
+				field: "email",
+				headerName: "Email",
+				type: "string",
+				flex: 1.25,
+			},
+			{
 				field: "groups",
 				headerName: "Groups",
-				flex: 2,
+				flex: 2.5,
 				editable: true,
 				renderCell: (params) => renderGroupsInput(params, false),
 				renderEditCell: (params) => renderGroupsInput(params, true),
 			},
 			{
-				field: "entryTime",
-				headerName: "Entry Time",
-				type: "dateTime",
-				flex: 1.25,
-			},
-			{
-				field: "cleared",
-				headerName: "Cleared",
-				type: "boolean",
-				flex: 1,
-				renderCell: renderClearedBadge,
-			},
-			{
 				field: "actions",
 				headerName: "Actions",
 				type: "actions",
-				flex: 2,
+				flex: 1,
 				getActions: (params: GridRowParams) => [
-					<Button>View full log</Button>,
 					<Button className="bg-orange-600 hover:bg-orange-700">Trace contacts</Button>,
 				],
 				align: "center",
@@ -101,7 +80,6 @@ const EntryLogTable: React.FC<{
 	const rows: GridRowModel[] = entries.map((entry) => ({
 		...entry,
 		groups: entry.groups.join("|,"),
-		entryTime: entry.dateCreated,
 	}));
 
 	return <DataGrid columns={columns} rows={rows} />;
