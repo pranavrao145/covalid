a27ecff66c20badb9fdde64197d62a589712ce96
Pranav Rao <56097527+pranavrao145@users.noreply.github.com>
2021-09-18 12:51:13 -0400

    feat(controllers): added groups controller
    All of the basic REST actions were added.

---
+++ server/src/controllers/groups.ts
@@ -0,0 +1,103 @@
+import { Request, Response } from "express";
+import { createConnection } from "typeorm";
+import { Group } from "../entities/Group";
+
+// GET /groups/:id
+export const getGroup = async (req: Request, res: Response) => {
+    // get a connection to the database
+    const connection = await createConnection();
+
+    // access the group repository
+    const repository = connection.getRepository(Group);
+
+    try {
+        // get the group with the uid specified
+        const group = await repository.findOne(req.params.id);
+        res.status(200).json(group);
+    } catch (e) {
+        res.status(404).json({ message: "An error occurred fetching this group." });
+    }
+}
+
+// GET /groups
+export const getAllGroups = async (_req: Request, res: Response) => {
+    // get a connection to the database
+    const connection = await createConnection();
+
+    // access the group repository
+    const repository = connection.getRepository(Group);
+
+    try {
+        // get all the groups in the database
+        const groups = await repository.find();
+        res.status(200).json(groups);
+    } catch (e) {
+        res.status(404).json({ message: "An error occurred fetching all groups." });
+    }
+}
+
+// POST /groups
+export const createGroup = async (req: Request, res: Response) => {
+    // get a connection to the database
+    const connection = await createConnection();
+
+    // access the group repository
+    const repository = connection.getRepository(Group);
+
+    try {
+        // create a new group with the given information
+        const group = repository.create(req.body);
+
+        // save the new group
+        const saveResults = await repository.save(group!);
+
+        res.status(200).json(saveResults);
+    } catch (e) {
+        res.status(404).json({ message: "An error occurred creating this group." });
+    }
+}
+
+// PUT /groups/:id
+export const updateGroup = async (req: Request, res: Response) => {
+    // get a connection to the database
+    const connection = await createConnection();
+
+    // access the group repository
+    const repository = connection.getRepository(Group);
+
+    try {
+        // get the group with the uid specified
+        const group = await repository.findOne(req.params.id);
+
+        // update the group data
+        repository.merge(group!, req.body);
+
+        // save the group with the new details
+        const saveResults = await repository.save(group!);
+
+        res.status(200).json(saveResults);
+    } catch (e) {
+        res.status(404).json({ message: "An error occurred updating this group." });
+    }
+}
+
+// DELETE /groups/:id
+export const deleteGroup = async (req: Request, res: Response) => {
+    // get a connection to the database
+    const connection = await createConnection();
+
+    // access the group repository
+    const repository = connection.getRepository(Group);
+
+    try {
+        // get the group with the uid specified
+        const group = await repository.findOne(req.params.id);
+ 
+        // delete the group 
+        const saveResults = await repository.delete(group!);
+
+        res.status(200).json(saveResults);
+    } catch (e) {
+        res.status(404).json({ message: "An error occurred deleting this group." });
+    }
+}
