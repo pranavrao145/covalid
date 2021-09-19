0d2082d3012c77c9cb97c23ea824f5af3fba8e34
Futuristick <me@futuristick.ca>
2021-09-18 22:09:50 -0400

    Finished contact tracing popup

--- client/src/components/dashboard/ContactTracingPopup.tsx
+++ client/src/components/dashboard/ContactTracingPopup.tsx
@@ -1,7 +1,7 @@
 import React, { Dispatch, Fragment, SetStateAction } from "react";
 import { Dialog, Transition } from "@headlessui/react";
 import { ShieldExclamationIcon } from "@heroicons/react/solid";
-import ContactTracingDropdown from "./ ContactTracingDropdown";
+import ContactTracingDropdown from "./ContactTracingDropdown";
 
 const ContactTracingPopup: React.FC<{
 	open: boolean;
@@ -37,7 +37,7 @@ const ContactTracingPopup: React.FC<{
 					leaveFrom="opacity-100 translate-y-0 sm:scale-100"
 					leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
 				>
-					<div className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full sm:p-6">
+					<div className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full sm:p-6">
 						<div>
 							<div className="mx-auto flex items-center justify-center h-12 w-12">
 								<ShieldExclamationIcon className="h-12 w-12 text-orange-600" aria-hidden="true" />
@@ -47,14 +47,28 @@ const ContactTracingPopup: React.FC<{
 									Trace contacts
 								</Dialog.Title>
 								<div className="mt-2">
-									<p className="text-sm text-gray-500">
-										Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur amet labore.
-									</p>
+									<p className="text-sm text-gray-500">Select a period of time to trace contacts.</p>
 								</div>
 							</div>
 						</div>
 						<div className="mt-5 sm:mt-6">
 							<ContactTracingDropdown tracingTimeframe={tracingTimeframe} setTracingTimeframe={setTracingTimeframe} />
+							<div className="mt-5 sm:mt-6 sm:grid sm:grid-cols-2 sm:gap-3 sm:grid-flow-row-dense">
+								<button
+									type="button"
+									className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-teal-600 text-base font-medium text-white hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teal-500 sm:col-start-2 sm:text-sm"
+									onClick={() => setOpen(false)}
+								>
+									Trace contacts
+								</button>
+								<button
+									type="button"
+									className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teal-500 sm:mt-0 sm:col-start-1 sm:text-sm"
+									onClick={() => setOpen(false)}
+								>
+									Cancel
+								</button>
+							</div>
 						</div>
 					</div>
 				</Transition.Child>
